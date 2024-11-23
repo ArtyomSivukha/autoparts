f1e871b5900d1d9e8b56f108483f94068cdf744b
newDmoon <20125dimanovogrodsky@gmail.com>
2024-11-24 01:00:12 +0300

    feat: cart, cartitems

--- src/main/java/com/example/autoparts/service/CartService.java
+++ src/main/java/com/example/autoparts/service/CartService.java
@@ -1,43 +1,53 @@
 package com.example.autoparts.service;
 
+import com.example.autoparts.model.AutoPart;
 import com.example.autoparts.model.Cart;
 import com.example.autoparts.model.CartItem;
+import com.example.autoparts.model.User;
+import com.example.autoparts.repository.AutoPartRepository;
 import com.example.autoparts.repository.CartRepository;
+import com.example.autoparts.repository.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.Data;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+import java.util.List;
 import java.util.Optional;
 
 @Service
 @Data
 public class CartService {
     private final CartRepository cartRepository;
+    private final UserService userService;
+    private final UserRepository userRepository;
+    private final AutoPartRepository autoPartRepository;
 
-    public Optional<Cart> getCartByUserId(Long userId) {
-        return cartRepository.findByUserId(userId);
+    public List<CartItem> getCartItemsFromCurrent() {
+        return userService.getCurrent().getCart().getItems();
     }
 
-    public Cart createCart(Cart cart) {
+    public Cart addCartItem(Long autoPartId) {
+        CartItem cartItem = new CartItem();
+        Cart cart = userService.getCurrent().getCart();
+        AutoPart autoPart = autoPartRepository.findById(autoPartId).get();
+        cartItem.setCart(cart);
+        cartItem.setPart(autoPart);
+        cartItem.setQuantity(1);
+        cartItem.setPrice(BigDecimal.valueOf(autoPart.getPrice()));
+        cart.getItems().add(cartItem);
         return cartRepository.save(cart);
     }
 
-    public Cart addCartItem(Long cartId, CartItem cartItem) {
-        return cartRepository.findById(cartId).map(cart -> {
-            cartItem.setCart(cart); // Связываем CartItem с корзиной
-            cart.getItems().add(cartItem);
-            return cartRepository.save(cart);
-        }).orElseThrow(() -> new RuntimeException("Cart not found"));
-    }
+    public CartItem deleteCartItem(Long autoPartID) {
+        Cart cart = userService.getCurrent().getCart();
 
-    public Cart removeCartItem(Long cartId, Long cartItemId) {
-        return cartRepository.findById(cartId).map(cart -> {
-            cart.getItems().removeIf(item -> item.getCartItemId().equals(cartItemId));
-            return cartRepository.save(cart);
-        }).orElseThrow(() -> new RuntimeException("Cart not found"));
-    }
-
-    public void deleteCart(Long cartId) {
-        cartRepository.deleteById(cartId);
+        CartItem itemToDelete = cart.getItems().stream()
+                .filter(item -> item.getPart().getId().equals(autoPartID))
+                .findFirst()
+                .orElseThrow(() -> new EntityNotFoundException("В корзине отсутствует"));
+        cart.getItems().remove(itemToDelete);
+        cartRepository.save(cart);
+        return itemToDelete;
     }
 }
